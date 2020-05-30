3d567924681bcfb297cd725c4aac7c6cb5e12580
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-05-30 12:55:53 -0300

    Remove unused import

--- DreamVanish/src/main/kotlin/net/perfectdreams/dreamvanish/DreamVanishAPI.kt
+++ DreamVanish/src/main/kotlin/net/perfectdreams/dreamvanish/DreamVanishAPI.kt
@@ -2,7 +2,6 @@ package net.perfectdreams.dreamvanish
 
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
-import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 import java.util.*
 
