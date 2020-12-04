d6215abc827b4d3f019988661be4b4654743be0d
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-12-04 11:09:17 -0300

    Remove broken user info methods

--- DreamCore/src/test/kotlin/net/perfectdreams/dreamcore/utils/DreamUtilsTest.kt
+++ DreamCore/src/test/kotlin/net/perfectdreams/dreamcore/utils/DreamUtilsTest.kt
@@ -11,30 +11,6 @@ import org.junit.jupiter.api.Test
 import java.util.*
 
 class DreamUtilsTest {
-	companion object {
-		const val TEST_USERNAME = "MrPowerGamerBR"
-		val TEST_UUID = UUID.nameUUIDFromBytes(("OfflinePlayer:$TEST_USERNAME").toByteArray(Charsets.UTF_8))
-	}
-
-	val player = mockk<Player>()
-
-	init {
-		every { player.name } returns TEST_USERNAME
-		every { player.uniqueId } returns TEST_UUID
-
-		mockkStatic("org.bukkit.Bukkit")
-		every { Bukkit.isPrimaryThread() } returns true
-	}
-
-	@Test
-	fun `test player info`() {
-		val playerInfo = DreamUtils.createPerfectDreamsPlayerInfo(player)
-
-		assertThat(playerInfo.username).isEqualTo(TEST_USERNAME)
-		assertThat(playerInfo.lowerCaseUsername).isEqualToIgnoringCase(TEST_USERNAME)
-		assertThat(playerInfo.uniqueId).isEqualTo(TEST_UUID)
-	}
-
 	@Test
 	fun `assert main thread in main thread`() {
 		DreamUtils.assertMainThread(true)
