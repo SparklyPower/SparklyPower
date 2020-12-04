19d0549ca0e47b42c8e7cfbba54cb84979618182
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-12-04 18:10:36 -0300

    Add "/np" as an aliases for "/naopegar"

--- DreamMini/src/main/kotlin/net/perfectdreams/dreammini/commands/DoNotPickupCommand.kt
+++ DreamMini/src/main/kotlin/net/perfectdreams/dreammini/commands/DoNotPickupCommand.kt
@@ -6,7 +6,7 @@ import net.perfectdreams.dreammini.DreamMini
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
-class DoNotPickupCommand(val m: DreamMini) : SparklyCommand(arrayOf("nãopegar", "naopegar", "donotpickup", "filtrardrops")) {
+class DoNotPickupCommand(val m: DreamMini) : SparklyCommand(arrayOf("nãopegar", "naopegar", "donotpickup", "filtrardrops", "np")) {
     @Subcommand
     fun doNotPickup(player: Player) {
         val inventory = m.dropsBlacklist.getOrPut(player) { Bukkit.createInventory(null, 54, "§5§lQuais itens §4§lNÃO§5§l serão pegos?") }
