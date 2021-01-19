dde4227bf68ea058d69685a7ded5dd2d5ad0e719
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-01-19 00:10:04 -0300

    Maybe this will work?

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ allprojects {
         maven("https://dl.bintray.com/kotlin/kotlin-eap/")
         maven("https://jcenter.bintray.com")
         maven("https://oss.sonatype.org/content/repositories/snapshots/")
-        maven("https://repo.perfectdreams.net/")
+        // maven("https://repo.perfectdreams.net/")
         maven("https://dl.bintray.com/kotlin/exposed/")
         maven("https://nexus.wesjd.net/repository/thirdparty/")
         // maven("http://nexus.okkero.com/repository/maven-releases/")
