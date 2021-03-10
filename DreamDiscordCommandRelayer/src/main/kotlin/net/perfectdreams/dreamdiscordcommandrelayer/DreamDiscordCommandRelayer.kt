4db81542ddc77a67860b9cce3003c139d0858509
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-03-10 16:59:06 -0300

    Change command relay to be batched instead of sending a different message every single time

--- DreamDiscordCommandRelayer/src/main/kotlin/net/perfectdreams/dreamdiscordcommandrelayer/DreamDiscordCommandRelayer.kt
+++ DreamDiscordCommandRelayer/src/main/kotlin/net/perfectdreams/dreamdiscordcommandrelayer/DreamDiscordCommandRelayer.kt
@@ -2,44 +2,76 @@ package net.perfectdreams.dreamdiscordcommandrelayer
 
 import club.minnced.discord.webhook.WebhookClient
 import club.minnced.discord.webhook.send.WebhookMessageBuilder
+import com.okkero.skedule.SynchronizationContext
+import com.okkero.skedule.schedule
+import kotlinx.coroutines.delay
 import net.perfectdreams.dreamcore.utils.KotlinPlugin
 import net.perfectdreams.dreamcore.utils.registerEvents
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerCommandPreprocessEvent
 import java.text.DecimalFormat
+import java.util.*
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.ConcurrentLinkedQueue
 
 class DreamDiscordCommandRelayer : KotlinPlugin(), Listener {
-	val commandWebhooks = mutableListOf<WebhookClient>()
-	var currentWebhookIdx = 0
+	val commandWebhook = WebhookClient.withUrl(config.getString("command-webhook")!!)
+	val queue = ConcurrentLinkedQueue<String>()
 
 	override fun softEnable() {
 		super.softEnable()
 
-		config.getStringList("command-webhooks").forEach {
-			commandWebhooks += WebhookClient.withUrl(it)
-		}
-
 		registerEvents(this)
+
+		launchAsyncThread {
+			while (true) {
+				val builder = StringBuilder()
+
+				while (queue.isNotEmpty()) {
+					val firstElement = queue.peek()
+
+					// Current length + First Element + "\n"
+					// If it will overflow, we break the loop and send the message as is
+					if (builder.length + firstElement.length + 1 > 2000)
+						break
+
+					// Append the message content
+					builder.append(firstElement)
+					builder.append("\n")
+
+					// And remove the current message!
+					queue.remove()
+				}
+
+				if (builder.isNotEmpty()) {
+					try {
+						// Send the message if the content is not empty
+						commandWebhook.send(
+							WebhookMessageBuilder()
+								.setUsername("Gabriela, a amiga dos comandos \uD83D\uDCBB")
+								.setAvatarUrl("https://cdn.discordapp.com/attachments/513405772911345664/769319309977583676/gabriela_avatar.png")
+								.setContent(builder.toString())
+								.build()
+						)
+					} catch (e: Exception) {
+						e.printStackTrace()
+					}
+				}
+
+				delay(1_000)
+			}
+		}
 	}
 
 	override fun softDisable() {
-		commandWebhooks.forEach { it.close() }
+		commandWebhook.close()
 	}
 
 	@EventHandler
 	fun onCommand(e: PlayerCommandPreprocessEvent) {
-		val currentWebhook = commandWebhooks[currentWebhookIdx % commandWebhooks.size]
-
 		val nf = DecimalFormat("##.##")
-		currentWebhook.send(
-			WebhookMessageBuilder()
-				.setUsername("Gabriela, a amiga dos comandos \uD83D\uDCBB")
-				.setAvatarUrl("https://cdn.discordapp.com/attachments/513405772911345664/769319309977583676/gabriela_avatar.png")
-				.setContent("[`${e.player.location.world.name}` » `${nf.format(e.player.location.x)}`, `${nf.format(e.player.location.y)}`, `${nf.format(e.player.location.z)}`] **${e.player.name}**: `${e.message}`")
-				.build()
-		)
 
-		currentWebhookIdx++
+		queue.add("[`${e.player.location.world.name}` » `${nf.format(e.player.location.x)}`, `${nf.format(e.player.location.y)}`, `${nf.format(e.player.location.z)}`] **${e.player.name}**: `${e.message}`")
 	}
 }
