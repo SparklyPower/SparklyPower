3652ec270158c0b83079b2bb19a9fab0dd64df2b
TodFoz <davidprofile.acc@gmail.com>
2021-06-30 15:26:35 -0300

    Fixed custom items recipe command and nether gold ore can now be smelted in the superfurnace.

--- bukkit/DreamCustomItems/src/main/kotlin/net/perfectdreams/dreamcustomitems/commands/CustomItemRecipeCommand.kt
+++ bukkit/DreamCustomItems/src/main/kotlin/net/perfectdreams/dreamcustomitems/commands/CustomItemRecipeCommand.kt
@@ -1,73 +1,120 @@
 package net.perfectdreams.dreamcustomitems.commands
 
 import net.perfectdreams.dreamcore.utils.commands.DSLCommandBase
+import net.perfectdreams.dreamcore.utils.createMenu
 import net.perfectdreams.dreamcustomitems.DreamCustomItems
 import net.perfectdreams.dreamcustomitems.holders.CustomItemRecipeHolder
 import net.perfectdreams.dreamcustomitems.utils.CustomItems
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.event.inventory.InventoryType
+import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.ShapedRecipe
-import javax.lang.model.element.VariableElement
-import kotlin.reflect.KType
-import kotlin.reflect.javaType
-import kotlin.reflect.typeOf
+import kotlin.reflect.KCallable
 
 object CustomItemRecipeCommand : DSLCommandBase<DreamCustomItems> {
     override fun command(plugin: DreamCustomItems) = create(
-            listOf("customitemrecipe")
+            listOf("customrecipes", "customitems")
     ) {
         executes {
             val itemName = args.getOrNull(0)
 
-            val customItems = CustomItems::class.members.filter {
-                it.returnType.toString().contains("ItemStack") && it.name != "RUBY"
-            }
+            val customItemsList = customItems(true)
 
             if (itemName == null) {
-                var customItemsNameList = ""
+                val invetorySize = if (customItemsList.size % 9 == 0) customItemsList.size
+                else customItemsList.size + 9 - (customItemsList.size % 9)
 
-                customItems.forEach { customItemsNameList += "§e${it.name.toLowerCase()}§f|" }
+                val menu = createMenu(invetorySize, "§cItens customizados") {
+                    for ((value, item) in customItemsList.withIndex()) {
+                        slot(value, 0) {
+                            val selectedItem = getNewCustomItem(item.name, customItemsList)
 
-                sender.sendMessage("§e/craft §f(${customItemsNameList.removeSuffix("|")})")
+                            this.item = selectedItem
 
-                return@executes
-            }
+                            onClick {
+                                it.closeInventory()
 
-            if (customItems.firstOrNull { it.name == itemName.toUpperCase() } == null) {
-                sender.sendMessage("§cEste item não existe!")
+                                val recipeInvetory = Bukkit.createInventory(
+                                        CustomItemRecipeHolder(),
+                                        InventoryType.WORKBENCH,
+                                        "Crafting"
+                                )
 
-                return@executes
-            }
+                                val key = NamespacedKey(plugin, item.name.toLowerCase())
+                                val itemRecipe = Bukkit.getServer().getRecipe(key) as ShapedRecipe
+                                val ruby = getNewCustomItem("RUBY", customItems(false))
 
-            val recipeInvetory = Bukkit.createInventory(CustomItemRecipeHolder(), InventoryType.WORKBENCH, "Crafting")
+                                recipeInvetory.setItem(0, selectedItem) // Recipe result
 
-            val key = NamespacedKey(plugin, itemName.toLowerCase())
+                                var inventoryIndex = 1
 
-            val itemRecipe = Bukkit.getServer().getRecipe(key) as ShapedRecipe
+                                itemRecipe.ingredientMap.forEach { ingredient ->
+                                    when {
+                                        ingredient.value == null -> {
+                                            recipeInvetory.setItem(inventoryIndex, ItemStack(Material.AIR))
+                                        }
+                                        ingredient.value.type == Material.PRISMARINE_SHARD -> {
+                                            recipeInvetory.setItem(inventoryIndex, ruby)
+                                        }
+                                        else -> {
 
-            val itemResult = customItems.first { it.name == itemName.toUpperCase() }.call(CustomItems) as ItemStack
+                                            ingredient.value.addUnsafeEnchantment(Enchantment.LUCK, 1)
+                                            ingredient.value.addItemFlags(ItemFlag.HIDE_ENCHANTS)
 
-            recipeInvetory.setItem(0, itemResult.clone())
+                                            recipeInvetory.setItem(inventoryIndex, ingredient.value)
+                                        }
+                                    }
 
-            var inventoryIndex = 1
+                                    inventoryIndex += 1
+                                }
 
-            itemRecipe.ingredientMap.forEach {
-
-                if (it.value == null) {
-                    recipeInvetory.setItem(inventoryIndex, ItemStack(Material.AIR))
-                } else if (it.value.type == Material.PRISMARINE_SHARD) {
-                    recipeInvetory.setItem(inventoryIndex, CustomItems.RUBY)
-                } else {
-                    recipeInvetory.setItem(inventoryIndex, it.value)
+                                player.openInventory(recipeInvetory)
+                            }
+                        }
+                    }
                 }
 
-                inventoryIndex += 1
+                menu.sendTo(player)
+                return@executes
             }
-
-			player.openInventory(recipeInvetory)
         }
+    }
+
+    fun customItems(ignoreNonCraftableItems: Boolean): List<KCallable<*>> {
+
+        return if (ignoreNonCraftableItems) {
+            CustomItems::class.members.filter {
+                it.returnType.toString().contains("ItemStack") && it.name != "RUBY"
+            }
+        } else {
+            CustomItems::class.members.filter {
+                it.returnType.toString().contains("ItemStack")
+            }
+        }
+    }
+
+    fun getNewCustomItem(itemName: String, customItems: List<KCallable<*>>): ItemStack {
+
+        val newItem = (customItems.first { it.name == itemName.toUpperCase() }.call(CustomItems) as ItemStack).clone()
+
+        val newItemMeta = newItem.itemMeta
+
+        if (newItem.type == Material.PRISMARINE_SHARD) {
+            newItemMeta.lore = listOf(
+                    "§7Rubí pode ser encontrado quebrando",
+                    "§7minérios de redstone, cada minério",
+                    "§7possui §8${CustomItems.RUBY_DROP_CHANCE}% §7de chance de",
+                    "§7dropar um rubi!")
+        }
+
+        newItemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS)
+
+        newItem.itemMeta = newItemMeta
+
+        return newItem
     }
 }
