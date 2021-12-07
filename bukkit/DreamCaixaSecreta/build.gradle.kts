c53655427b55c63b1633f3b47bb4bfeb330f83bc
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-12-07 15:54:13 -0300

    Whoops, forgot to change the JAR name in the reobfTask

--- bukkit/DreamCaixaSecreta/build.gradle.kts
+++ bukkit/DreamCaixaSecreta/build.gradle.kts
@@ -22,6 +22,12 @@ tasks.withType<KotlinCompile> {
 }
 
 tasks {
+    reobfJar {
+        // For some reason the userdev plugin is using "unspecified" as the suffix, and that's not a good name
+        // So we are going to change it to "PluginName-reobf.jar"
+        outputJar.set(layout.buildDirectory.file("libs/${project.name}-reobf.jar"))
+    }
+    
     // Configure reobfJar to run when invoking the build task
     build {
         dependsOn(reobfJar)
