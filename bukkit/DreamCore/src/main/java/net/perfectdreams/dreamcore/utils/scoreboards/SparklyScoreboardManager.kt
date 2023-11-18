338e824fb1e9f2de463b299dd5011467883b010a
MrPowerGamerBR <git@mrpowergamerbr.com>
2023-11-18 02:05:29 -0300

    Remove scoreboard clean up code
    Because we removed the HEALTH criteria from DreamScoreboard, the scoreboards aren't tracked by the vanilla scoreboards anymore, so we don't need this code

--- bukkit/DreamCore/src/main/java/net/perfectdreams/dreamcore/utils/scoreboards/SparklyScoreboardManager.kt
+++ bukkit/DreamCore/src/main/java/net/perfectdreams/dreamcore/utils/scoreboards/SparklyScoreboardManager.kt
@@ -15,50 +15,7 @@ import org.bukkit.scoreboard.Scoreboard
 import java.util.concurrent.ConcurrentHashMap
 
 class SparklyScoreboardManager(val m: DreamCore) {
-    companion object {
-        // 13/07/2021 - YES, THIS IS NEEDED, PAPER HASN'T FIXED THIS ISSUE YET
-        // THERE IS A PATCH THAT IMPLEMENTS SCOREBOARD CLEAN UP, BUT THAT DOESN'T FIX OUR USE CASE!!
-        // https://canary.discord.com/channels/@me/646488209274044457/864264697356091393
-        private val scoreboardsField by lazy {
-            (Bukkit.getScoreboardManager() as CraftScoreboardManager)::class.java.getDeclaredField("scoreboards").apply {
-                this.isAccessible = true
-            }
-        }
-
-        private val playerBoardsField by lazy {
-            (Bukkit.getScoreboardManager() as CraftScoreboardManager)::class.java.getDeclaredField("playerBoards")
-                .apply {
-                    this.isAccessible = true
-                }
-        }
-
-        fun cleanUp(scoreboard: Scoreboard) {
-            // Needs to be fixed: https://github.com/PaperMC/Paper/issues/4260
-            val scoreboards = scoreboardsField.get(Bukkit.getScoreboardManager()) as WeakCollection<Scoreboard>
-            scoreboards.remove(scoreboard)
-        }
-    }
-
     internal val scoreboards = ConcurrentHashMap<Player, PhoenixScoreboard>()
-
-    fun startScoreboardCleanUpTask() {
-        // Needs to be fixed: https://github.com/PaperMC/Paper/issues/4260
-        m.launchMainThread {
-            while (true) {
-                val weakCollection = scoreboardsField.get(Bukkit.getScoreboardManager()) as WeakCollection<CraftScoreboard>
-
-                m.logger.info("Weak Collection before clean up size is ${weakCollection.size}")
-
-                val playerBoards = playerBoardsField.get(Bukkit.getScoreboardManager()) as Map<CraftPlayer, CraftScoreboard>
-
-                weakCollection.removeAll((weakCollection - playerBoards.values).toSet())
-
-                m.logger.info("Weak Collection after clean up size is ${weakCollection.size}")
-
-                delayTicks(20 * 60) // 1 minute
-            }
-        }
-    }
 
     fun getScoreboard(player: Player) = scoreboards[player]
 
@@ -76,12 +33,6 @@ class SparklyScoreboardManager(val m: DreamCore) {
     }
 
     fun removeScoreboard(player: Player) {
-        // Manually clean up scoreboards
-        val playerScoreboard = scoreboards[player]
-        playerScoreboard?.let {
-            cleanUp(it.scoreboard)
-        }
-
         scoreboards.remove(player)
 
         Bukkit.getPluginManager().callEvent(PlayerScoreboardRemovedEvent(player))
