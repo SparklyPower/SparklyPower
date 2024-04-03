d3111740f3fd6ff6f24cae8c7c0e878a0b1b957d
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-04-03 14:50:40 -0300

    Use the proper item name instead of the enum name

--- bukkit/DreamQuickHarvest/src/main/kotlin/net/perfectdreams/dreamquickharvest/commands/ColheitaExecutor.kt
+++ bukkit/DreamQuickHarvest/src/main/kotlin/net/perfectdreams/dreamquickharvest/commands/ColheitaExecutor.kt
@@ -1,7 +1,9 @@
 package net.perfectdreams.dreamquickharvest.commands
 
+import net.kyori.adventure.text.format.NamedTextColor
 import net.perfectdreams.dreamcore.utils.Databases
 import net.perfectdreams.dreamcore.utils.DateUtils
+import net.perfectdreams.dreamcore.utils.TranslationUtils
 import net.perfectdreams.dreamcore.utils.commands.context.CommandArguments
 import net.perfectdreams.dreamcore.utils.commands.context.CommandContext
 import net.perfectdreams.dreamcore.utils.commands.executors.SparklyCommandExecutor
@@ -12,6 +14,7 @@ import net.perfectdreams.dreamquickharvest.tables.PlayerQuickHarvestUpgrades
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
+import protocolsupport.api.TranslationAPI
 import java.time.Instant
 
 class ColheitaExecutor(val m: DreamQuickHarvest) : SparklyCommandExecutor() {
@@ -28,7 +31,9 @@ class ColheitaExecutor(val m: DreamQuickHarvest) : SparklyCommandExecutor() {
                 context.sendMessage("§aVocê ganha §e${m.howManyQuickHarvestBlocksThePlayerShouldEarn(player)} energias§a de colheita por segundo. Ao subir nível de herbalismo, a sua colheita rápida aumenta mais rápido (Até o Nível ${DreamQuickHarvest.HERBALISM_LEVEL_CAP})")
                 context.sendMessage("§aCada bloco de plantação usam energias diferentes, sendo eles...")
                 DreamQuickHarvest.BLOCK_ENERGY_COST.forEach {
-                    context.sendMessage("§e${it.key}§a: ${it.value}")
+                    val translationKey = it.key.blockTranslationKey ?: it.key.itemTranslationKey
+                    val translatedName = translationKey?.let { TranslationAPI.getTranslationString("pt_br", it) } ?: it.key.name
+                    context.sendMessage("§e${translatedName.uppercase()}§a: ${it.value}")
                 }
                 context.sendMessage("§aVocê pode aumentar o seu limite de energia da colheita rápida com pesadelos! §e/colheita upgrade")
                 context.sendMessage("§aUpgrades ativos:")
@@ -40,8 +45,21 @@ class ColheitaExecutor(val m: DreamQuickHarvest) : SparklyCommandExecutor() {
                     }
 
                     onMainThread {
-                        for (upgrade in upgrades) {
-                            context.sendMessage("§6+§e${upgrade[PlayerQuickHarvestUpgrades.energy]} energias §8(§7${DateUtils.formatDateDiff(upgrade[PlayerQuickHarvestUpgrades.expiresAt].toEpochMilli())}§8)")
+                        if (upgrades.isEmpty()) {
+                            context.sendMessage {
+                                color(NamedTextColor.GRAY)
+                                content("Vazio...")
+                            }
+                        } else {
+                            for (upgrade in upgrades) {
+                                context.sendMessage(
+                                    "§6+§e${upgrade[PlayerQuickHarvestUpgrades.energy]} energias §8(§7${
+                                        DateUtils.formatDateDiff(
+                                            upgrade[PlayerQuickHarvestUpgrades.expiresAt].toEpochMilli()
+                                        )
+                                    }§8)"
+                                )
+                            }
                         }
                     }
                 }
