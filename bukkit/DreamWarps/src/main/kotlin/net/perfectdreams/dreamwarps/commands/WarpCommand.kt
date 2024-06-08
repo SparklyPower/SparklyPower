073a2ab5f60a9d62d2005ea913b547d9502255e1
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-06-08 02:59:36 -0300

    Add autocomplete to warp names

--- bukkit/DreamWarps/src/main/kotlin/net/perfectdreams/dreamwarps/commands/WarpCommand.kt
+++ bukkit/DreamWarps/src/main/kotlin/net/perfectdreams/dreamwarps/commands/WarpCommand.kt
@@ -1,44 +1,66 @@
 package net.perfectdreams.dreamwarps.commands
 
 import net.perfectdreams.dreamcore.utils.TextUtils
-import net.perfectdreams.dreamcore.utils.commands.AbstractCommand
-import net.perfectdreams.dreamcore.utils.commands.annotation.Subcommand
+import net.perfectdreams.dreamcore.utils.commands.context.CommandArguments
+import net.perfectdreams.dreamcore.utils.commands.context.CommandContext
+import net.perfectdreams.dreamcore.utils.commands.declarations.SparklyCommandDeclarationWrapper
+import net.perfectdreams.dreamcore.utils.commands.declarations.sparklyCommand
+import net.perfectdreams.dreamcore.utils.commands.executors.SparklyCommandExecutor
+import net.perfectdreams.dreamcore.utils.commands.options.CommandOptions
 import net.perfectdreams.dreamcore.utils.extensions.playTeleportEffects
+import net.perfectdreams.dreamcore.utils.withoutPermission
 import net.perfectdreams.dreamwarps.DreamWarps
-import org.bukkit.entity.Player
 
-class WarpCommand(val m: DreamWarps) : AbstractCommand("warp", listOf("warps", "dwarps")) {
-	@Subcommand
-	fun root(player: Player) {
-		m.warpsMenu.sendTo(player)
+class WarpCommand(val m: DreamWarps) : SparklyCommandDeclarationWrapper {
+	override fun declaration() = sparklyCommand(listOf("warp", "warps", "dwarps")) {
+		executor = WarpExecutor(m)
 	}
 
-	@Subcommand
-	fun teleport(player: Player, name: String) {
-		val warp = m.warps.firstOrNull { it.name.equals(name, true) }
-
-		if (warp == null) {
-			player.sendMessage("${DreamWarps.PREFIX} §cNão existe nenhuma warp chamada ${name}!")
-			return
+	class WarpExecutor(val m: DreamWarps) : SparklyCommandExecutor() {
+		inner class Options : CommandOptions() {
+			val warpName = optionalGreedyString("warp_names") { context, builder ->
+				for (warp in m.warps) {
+					builder.suggest(warp.name)
+				}
+			}
 		}
 
-		if (!player.hasPermission("dreamwarps.warp.$name")) {
-			player.sendMessage("${DreamWarps.PREFIX} $withoutPermission")
-			return
-		}
+		override val options = Options()
 
-		player.teleportAsync(warp.location).thenRun {
-			player.playTeleportEffects()
-			player.sendMessage("${DreamWarps.PREFIX} §aVocê chegou ao seu destino. §cʕ•ᴥ•ʔ")
-			player.sendTitle(
-				"§b${warp.fancyName}",
-				"§3${TextUtils.ROUND_TO_2_DECIMAL.format(warp.location.x)}§b, §3${TextUtils.ROUND_TO_2_DECIMAL.format(
-					warp.location.y
-				)}§b, §3${TextUtils.ROUND_TO_2_DECIMAL.format(warp.location.z)}",
-				10,
-				60,
-				10
-			)
+		override fun execute(context: CommandContext, args: CommandArguments) {
+			val player = context.requirePlayer()
+			val name = args[options.warpName]
+
+			if (name == null) {
+				m.warpsMenu.sendTo(player)
+				return
+			}
+
+			val warp = m.warps.firstOrNull { it.name.equals(name, true) }
+
+			if (warp == null) {
+				context.sendMessage("${DreamWarps.PREFIX} §cNão existe nenhuma warp chamada ${name}!")
+				return
+			}
+
+			if (!player.hasPermission("dreamwarps.warp.$name")) {
+				player.sendMessage("${DreamWarps.PREFIX} $withoutPermission")
+				return
+			}
+
+			player.teleportAsync(warp.location).thenRun {
+				player.playTeleportEffects()
+				player.sendMessage("${DreamWarps.PREFIX} §aVocê chegou ao seu destino. §cʕ•ᴥ•ʔ")
+				player.sendTitle(
+					"§b${warp.fancyName}",
+					"§3${TextUtils.ROUND_TO_2_DECIMAL.format(warp.location.x)}§b, §3${TextUtils.ROUND_TO_2_DECIMAL.format(
+						warp.location.y
+					)}§b, §3${TextUtils.ROUND_TO_2_DECIMAL.format(warp.location.z)}",
+					10,
+					60,
+					10
+				)
+			}
 		}
 	}
 }
