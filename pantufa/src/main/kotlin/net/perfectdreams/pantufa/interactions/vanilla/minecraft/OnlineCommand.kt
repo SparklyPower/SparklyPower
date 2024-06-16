47763aaad0a6cfcb7e5bb05538e86ed9f7515956
hechfx <hechfx@hotmail.com>
2024-06-16 15:55:09 -0300

    Fixed `OnlineCommand.kt` bug when there were many players, the message wouldn't send due Discord's character limit.

--- pantufa/src/main/kotlin/net/perfectdreams/pantufa/interactions/vanilla/minecraft/OnlineCommand.kt
+++ pantufa/src/main/kotlin/net/perfectdreams/pantufa/interactions/vanilla/minecraft/OnlineCommand.kt
@@ -2,16 +2,17 @@ package net.perfectdreams.pantufa.interactions.vanilla.minecraft
 
 import com.github.salomonbrys.kotson.*
 import com.google.gson.JsonObject
+import dev.minn.jda.ktx.messages.Embed
+import net.dv8tion.jda.api.entities.emoji.Emoji
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle
 import net.dv8tion.jda.api.utils.FileUpload
-import net.perfectdreams.loritta.cinnamon.emotes.Emotes
 import net.perfectdreams.loritta.common.commands.CommandCategory
 import net.perfectdreams.loritta.morenitta.interactions.UnleashedContext
 import net.perfectdreams.loritta.morenitta.interactions.commands.*
 import net.perfectdreams.loritta.morenitta.interactions.commands.options.OptionReference
 import net.perfectdreams.pantufa.utils.Constants.SPARKLYPOWER_OFFLINE
-import net.perfectdreams.pantufa.api.commands.PantufaReply
 import net.perfectdreams.pantufa.utils.socket.SocketUtils
-import net.perfectdreams.pantufa.api.commands.styled
+import net.perfectdreams.pantufa.utils.Constants
 
 class OnlineCommand : SlashCommandDeclarationWrapper {
     companion object {
@@ -43,15 +44,10 @@ class OnlineCommand : SlashCommandDeclarationWrapper {
                 port = sparklyPower.server.perfectDreamsBungeePort,
                 success = { response ->
                     val servers = response["servers"].array
-                    val replies = mutableListOf<PantufaReply>()
                     val totalPlayersOnline = servers.sumOf { it["players"].array.size() }
-
-                    replies.add(
-                        PantufaReply(
-                            content = "**Players Online no SparklyPower Network ($totalPlayersOnline players online)**",
-                            prefix = Emotes.PantufaPickaxe.asMention
-                        )
-                    )
+                    val survivalPlayers = mutableListOf<String>()
+                    val lobbyPlayers = mutableListOf<String>()
+                    var page = 0
 
                     servers.forEach { jArr ->
                         val obj = jArr.obj
@@ -60,23 +56,12 @@ class OnlineCommand : SlashCommandDeclarationWrapper {
                             it["name"].string
                         }.sorted()
 
-                        val fancyName = prettyNameServer[name]
-
-                        if (fancyName != null) {
-                            if (players.isNotEmpty()) {
-                                replies.add(
-                                    PantufaReply(
-                                        "**$fancyName (${players.size})**: ${players.joinToString(", ", transform = { "**`$it`**" })}",
-                                        mentionUser = false
-                                    )
-                                )
-                            } else {
-                                replies.add(
-                                    PantufaReply(
-                                        "**$fancyName (0)**: Ninguém online... \uD83D\uDE2D",
-                                        mentionUser = false
-                                    )
-                                )
+                        when (name) {
+                            "sparklypower_survival" -> {
+                                survivalPlayers.addAll(players)
+                            }
+                            "sparklypower_lobby" -> {
+                                lobbyPlayers.addAll(players)
                             }
                         }
                     }
@@ -86,10 +71,43 @@ class OnlineCommand : SlashCommandDeclarationWrapper {
                             context.pantufa.playersOnlineGraph.getCachedGraph().inputStream()
                         else null
 
+                        val survivalPages = survivalPlayers.chunked(50)
+                        val lobbyPages = lobbyPlayers.chunked(50)
+
                         context.reply(false) {
-                            for (reply in replies) {
-                                styled(reply)
-                            }
+                            embeds += buildEmbed(totalPlayersOnline, survivalPlayers, survivalPages, lobbyPlayers, lobbyPages, page)
+
+                            actionRow(
+                                context.pantufa.interactivityManager
+                                    .buttonForUser(context.user, ButtonStyle.PRIMARY, builder = {
+                                        this.emoji = Emoji.fromCustom(
+                                            Constants.LEFT_EMOJI.name!!,
+                                            Constants.LEFT_EMOJI.id!!.value.toLong(),
+                                            Constants.LEFT_EMOJI.animated.discordBoolean
+                                        )
+                                    }) {
+                                        page--
+
+                                        it.deferAndEditOriginal {
+                                            embeds += buildEmbed(totalPlayersOnline, survivalPlayers, survivalPages, lobbyPlayers, lobbyPages, page)
+                                        }
+                                    },
+                                context.pantufa.interactivityManager
+                                    .buttonForUser(context.user, ButtonStyle.PRIMARY, builder = {
+                                        this.emoji = Emoji.fromCustom(
+                                            Constants.RIGHT_EMOJI.name!!,
+                                            Constants.RIGHT_EMOJI.id!!.value.toLong(),
+                                            Constants.RIGHT_EMOJI.animated.discordBoolean
+                                        )
+                                    }) {
+                                        page++
+
+                                        it.deferAndEditOriginal {
+                                            embeds += buildEmbed(totalPlayersOnline, survivalPlayers, survivalPages, lobbyPlayers, lobbyPages, page)
+                                        }
+                                    }
+
+                            )
 
                             if (playerOnlineGraphImage != null) {
                                 files += FileUpload.fromData(playerOnlineGraphImage, "graph.png")
@@ -109,6 +127,36 @@ class OnlineCommand : SlashCommandDeclarationWrapper {
             args: List<String>
         ): Map<OptionReference<*>, Any?> {
             return LorittaLegacyMessageCommandExecutor.NO_ARGS
+        }
+
+        private fun buildEmbed(
+            totalPlayersOnline: Int,
+            survivalPlayers: List<String>,
+            survivalPages: List<List<String>>,
+            lobbyPlayers: List<String>,
+            lobbyPages: List<List<String>>,
+            page: Int
+        ) = Embed {
+            title = "**Players Online no SparklyPower Network ($totalPlayersOnline players online)**"
+            color = Constants.LORITTA_AQUA.rgb
+
+            field {
+                name = "SparklyPower Survival (${survivalPlayers.size})"
+                value = if (survivalPlayers.isNotEmpty()) {
+                    survivalPages[page].joinToString(", ", transform = { "**`$it`**" })
+                } else {
+                    "Ninguém online... \uD83D\uDE2D"
+                }
+            }
+
+            field {
+                name = "SparklyPower Lobby (${lobbyPlayers.size})"
+                value = if (lobbyPlayers.isNotEmpty()) {
+                    lobbyPages[page].joinToString(", ", transform = { "**`$it`**" })
+                } else {
+                    "Ninguém online... \uD83D\uDE2D"
+                }
+            }
         }
     }
 }
