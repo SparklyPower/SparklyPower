93a9c32b5f4c3ae8854aa44d9951043a9a8b408e
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-06-19 01:24:15 -0300

    Derp

--- pantufa/src/main/kotlin/net/perfectdreams/pantufa/PantufaTasks.kt
+++ pantufa/src/main/kotlin/net/perfectdreams/pantufa/PantufaTasks.kt
@@ -2,7 +2,6 @@ package net.perfectdreams.pantufa
 
 import net.perfectdreams.pantufa.threads.CheckDreamPresenceTask
 import net.perfectdreams.pantufa.threads.SyncRolesTask
-import net.perfectdreams.pantufa.threads.UpdateCachedGraphs
 import net.perfectdreams.pantufa.threads.UpdatePantufaDiscordActivityTask
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
@@ -11,13 +10,6 @@ class PantufaTasks(private val pantufa: PantufaBot) {
     private val executorService = Executors.newScheduledThreadPool(4)
 
     fun start() {
-        executorService.scheduleWithFixedDelay(
-            UpdateCachedGraphs(pantufa),
-            0L,
-            1L,
-            TimeUnit.MINUTES
-        )
-
         executorService.scheduleWithFixedDelay(
             CheckDreamPresenceTask(),
             0L,
